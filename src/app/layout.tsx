11c2280a9ba8df3ecf9c3a52e1ce2629089b3a9f
jucastrum <juzokacastro@gmail.com>
2025-05-02 16:59:54 -0400

    🖌️ ajeitando fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import { ReactNode } from "react";
+import { ReactNode } from "react";  
 import Head from "next/head";
-import "./globals.css"; // Garantir que o globals.css seja importado
+import "./globals.css";  // Garantir que o globals.css seja importado
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Breathing+Personal+Use+Only&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=SF+Pro+Text:wght@600&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=SF+UI+Display:wght@600;700&display=swap"
-          rel="stylesheet"
-        />
+        <title>AjudarVocê - Ajuda em saúde mental</title>
+        <meta name="description" content="O AjudarVocê conecta pessoas de Manaus a apoio psicológico gratuito." />
+        <meta name="keywords" content="manaus, saude mental, psicólogo, ajuda" />
+        <meta name="author" content="AjudarVocê" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="AjudarVocê - Sua ajuda em saúde mental" />
+        <meta property="og:description" content="O AjudarVocê conecta pessoas de Manaus a apoio psicológico gratuito." />
+        <meta property="og:image" content="/images/og-image.png" />
+        <meta property="og:url" content="https://ajudarvoce.vercel.app/" />
       </Head>
-      <body>{children}</body>
+      
+      <body> {/* Não precisa aplicar a fonte diretamente aqui, já que isso foi feito no globals.css */}
+        {children}  {/* Renderiza os componentes filhos */}
+      </body>
     </html>
   );
 }
