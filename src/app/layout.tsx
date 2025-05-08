6ffc7fef06d398b7e89088fb38b002674408a05b
jucastrum <juzokacastro@gmail.com>
2025-05-08 16:36:35 -0400

    🧩Att metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from "react";  
-import Head from "next/head";
 import "./globals.css";  // Garantir que o globals.css seja importado
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className="scroll-smooth">
-      <Head>
         <title>AjudarVocê - Ajuda em saúde mental</title>
         <meta name="description" content="O AjudarVocê conecta pessoas de Manaus a apoio psicológico gratuito." />
         <meta name="keywords" content="manaus, saude mental, psicólogo, ajuda" />
@@ -14,9 +12,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <meta property="og:title" content="AjudarVocê - Sua ajuda em saúde mental" />
         <meta property="og:description" content="O AjudarVocê conecta pessoas de Manaus a apoio psicológico gratuito." />
         <meta property="og:image" content="/images/og-image.png" />
-        <meta property="og:url" content="https://ajudarvoce.vercel.app/" />
-      </Head>
-      
+        <meta property="og:url" content="https://ajudarvoce.vercel.app/" /> 
       <body> {/* Não precisa aplicar a fonte diretamente aqui, já que isso foi feito no globals.css */}
         {children}  {/* Renderiza os componentes filhos */}
       </body>
